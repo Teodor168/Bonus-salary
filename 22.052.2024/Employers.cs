3c81f994c74cae0f226de9e7dd86cfd4f62a0425
Teodor168 <teodor16iliev@gmail.com>
2024-05-12 23:05:05 +0300

    neshto

---
+++ 22.052.2024/Employers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._052._2024
+{
+    public class Employers
+    {
+        public Employers()
+        {
+            
+        }
+
+        private string name;
+        private double salary;
+        private string ocupation;
+        private string department;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+        public string Occupation
+        {
+            get { return ocupation; }
+            set { ocupation = value; }
+        }
+        public string Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name: {this.Name}; Salary: {Math.Round(this.Salary, 2)}; Occupation: {this.Occupation}; Department: {this.Department}");
+        }
+        public static void BonusSalary(List<Employers> people, double bonus)
+        {
+            for (int i = 0; i < people.Count; i++)
+            {
+                double bonusReal = (bonus / 100) * people[i].Salary;
+                people[i].Salary += bonusReal;
+            }
+        }
+    }
+}
---
+++ 22.052.2024/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _22._052._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employers> people = new List<Employers>();
+            Console.WriteLine("Kolko pari: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Employers person = new Employers();
+
+                Console.Write($"ime {i + 1}: ");
+                person.Name = Console.ReadLine();
+
+                Console.Write($"salary {i + 1}: ");
+                person.Salary = double.Parse(Console.ReadLine());
+
+                Console.Write($"occupation {i + 1}: ");
+                person.Occupation = Console.ReadLine();
+
+                Console.Write($"department {i + 1}: ");
+                person.Department = Console.ReadLine();
+
+                people.Add(person);
+            }
+            Console.WriteLine();
+            Console.Write("Kolko pari dobavqme: ");
+            double bonus = double.Parse(Console.ReadLine());
+            Employers.BonusSalary(people, bonus);
+            for (int i = 0; i < n; i++)
+            {
+                people[i].Print();
+            }
+        }
+    }
+}
